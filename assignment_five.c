bb56fb38a301b54cb8c4ceba2ec8af8226b9c6ee
John Glatts <33476958+jglatts@users.noreply.github.com>
2018-11-18 18:55:25 +0000

    Update assignment_five.c

--- assignment_five.c
+++ assignment_five.c
@@ -13,7 +13,8 @@ Due Date: 26 November 2018
 
 
 /* Function Prototypes */
-void enter_grades(char [][50], int[], int);
+void enter_grades(char [][50], float[], int);
+void print_report(char [][50], float[], int);
 
 
 int main(int argc, char const *argv[])
@@ -28,7 +29,7 @@ int main(int argc, char const *argv[])
 
 	// declare string arrays for all the names
 	char student_names[student_size][50];
-	int student_grades[student_size];	
+	float student_grades[student_size];	
 
 	// enter all the names and grades
 	enter_grades(student_names, student_grades, student_size);
@@ -38,9 +39,10 @@ int main(int argc, char const *argv[])
 }
 
 
-void enter_grades(char names[][50], int grades[], int size)
+/* Enter student grades and sort them as they're entered */
+void enter_grades(char names[][50], float grades[], int size)
 {
-	int temp;
+	float temp;
 	char temp_names[size][50];   
 
 	for (int i = 0; i < size; ++i)
@@ -48,9 +50,9 @@ void enter_grades(char names[][50], int grades[], int size)
 		// ask for student name first
 		printf("\nEnter student name\n --> ");
 		scanf(" %[^\n]s", names[i]);   // space has to be in front for the scanf() to work 
-		// get grades and sort as they're entered
+		// get grades and sort from highest to lowest
 		printf("\nEnter grade for %s\n --> ", names[i]);
-		scanf("%d", &grades[i]); 
+		scanf("%f", &grades[i]); 
 
 		if ( i >= 1) {
 			if (grades[i] > grades[i - 1]) {
@@ -64,23 +66,27 @@ void enter_grades(char names[][50], int grades[], int size)
 
 			}				
 
-		/* sort the grades, and keep track of the names
-		while ( i >= 1 ) {
-			if (grades[i] < grades[i - 1])
-			{
-				temp = grades[i];
-				grades[i] = grades[i - 1];
-				grades[i - 1] = temp;
-				break;
-			}
-		}
-		*/
 	}
 
-	// test print
-	for (int test = 0; test < size; ++test)
+	// once all grades are entered, print formated report
+	print_report(names, grades, size);
+
+}
+
+/* Print a nice report and call other helper functions to get information */
+void print_report(char names[][50], float grades[], int size)
+{
+	float average, median, lowest, highest;
+
+	printf("\n\n\n__________________________________________________\n");
+	printf("\n\tSorted Test Results");
+	printf("\n__________________________________________________\n");
+
+	for (int i = 0; i < size; ++i)
 	{
-		printf("\nName: %s            Grade: %d\n", names[test], grades[test]);
+		printf("\nName: %s              Grade: %0.1f\n", names[i], grades[i]);
 	}
 
+	//call other functions to return information
+	// average, median, highest, lowest, and name of highest score / name of lowest score
 }
