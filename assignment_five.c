2ba4b344b75ef12e1795d754788c56191d99afeb
John Glatts <33476958+jglatts@users.noreply.github.com>
2018-11-18 00:54:20 +0000

    Update assignment_five.c

--- assignment_five.c
+++ assignment_five.c
@@ -12,13 +12,6 @@ Due Date: 26 November 2018
 #include <time.h>
 
 
-struct StudentNames
-{
-		char student_names[student_size][50]; 	// declare a 2-D array of strings to hold student names 
-		int student_grades[student_size]; // size of the actual grades array will be same size as the amount of students
-};
-
-
 /* Test helper func() because of the buffer overflow */
 void enter_grades(int grades[], int index)
 {
@@ -42,23 +35,24 @@ void enter_grades(int grades[], int index)
 void enter_names(char names[][50], int grades[], int size)
 {
 	int temp;
+	char temp_array[size];
 
 	// SORT the grades as they're entered
 	// skips over the first name\grade pair, damnit
-	for (int i = 0; i <= size; ++i)
+	for (int i = 0; i < size; ++i)
 	{
 		// add seperate scanf() for number grade
 		printf("\nEnter Name...\n --> ");
 		fgets(names[i], 150, stdin);
-		fflush(stdin);
-		//fgets(&grades[i], 150, stdin); // will this work for int?
-		enter_grades(grades, i);
+		//fflush(stdin);
+		fgets(temp_array[i], 10, stdin); // will this work for int?
+		grades[i] = atoi(temp_array[i]);
 	}
 
 	printf("\n");
 
 	// test print
-	for (int test = 0; test <= size; ++test)
+	for (int test = 0; test < size; ++test)
 	{
 		printf("\nName: %s -- Grade: %d;\n", names[test], grades[test]);
 	}
@@ -68,13 +62,15 @@ void enter_names(char names[][50], int grades[], int size)
 int main(int argc, char const *argv[])
 {
 	int student_size;
-
+	char student_grades[student_size]; // size of the actual grades array will be same size as the amount of students
+	char student_names[student_size][50]; 	// declare a 2-D array of strings to hold student names 
+	
 	printf("__________________________________________________\n");
 	printf("\n\tStudent Test Results Report");
 	printf("\n__________________________________________________\n");
 	printf("\n\nHow many students are being added??\n --> ");
 	scanf("%d", &student_size);
-	enter_grades
+	enter_grades(student_names, student_grades, student_size);
 
 	/*
 	do
