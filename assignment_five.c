2fa50c8167a9efcec8bef505ae36f01c91b475d4
John Glatts <33476958+jglatts@users.noreply.github.com>
2018-11-22 08:11:01 -0500

    working on windows machine now, had to initialize the arrays with arbitrary values

--- assignment_five.c
+++ assignment_five.c
@@ -1,6 +1,6 @@
 /*
 
-CIS 1057 Assignment 5 
+CIS 1057 Assignment 5
 Author: John Glatts
 TA: Fang
 Due Date: 26 November 2018
@@ -15,89 +15,122 @@ Due Date: 26 November 2018
 /* Function Prototypes */
 void enter_grades(char [][50], float[], int);
 void print_report(char [][50], float[], int);
+float average_grade(float[], int);
+float median_grade(float[], int);
 
 
 int main(int argc, char const *argv[])
 {
-	int student_size;
+    int student_size;
 
-	printf("__________________________________________________\n");
-	printf("\n\tStudent Test Results Report");
-	printf("\n__________________________________________________\n");
-	printf("\n\nHow many students are being added??\n --> ");
-	scanf("%d", &student_size);
+    printf("__________________________________________________\n");
+    printf("\n\tStudent Test Results Report");
+    printf("\n__________________________________________________\n");
+    printf("\n\nHow many students are being added??\n --> ");
+    scanf("%d", &student_size);
 
-	// declare string arrays for all the names
-	char student_names[student_size][50];
-	float student_grades[student_size];	
+    // declare string arrays for all the names
+    // have to declare these with integers, windows has a compiler error if initialize with student_size (works on linux)
+    char student_names[50][50];
+    float student_grades[50];
 
-	// enter all the names and grades
-	enter_grades(student_names, student_grades, student_size);
+    // enter all the names and grades
+    enter_grades(student_names, student_grades, student_size);
 
 
-	return 0;
+    return 0;
 }
 
 
 /* Enter student grades and sort them as they're entered */
 void enter_grades(char names[][50], float grades[], int size)
 {
-	float temp;
-	char temp_names[size][50];   
+    float temp;
+    char temp_names[50][50];
 
-	for (int i = 0; i < size; ++i)
-	{
-		// ask for student name first
-		printf("\nEnter student name\n --> ");
-		scanf(" %[^\n]s", names[i]);   // space has to be in front for the scanf() to work 
-		// get grades and sort from highest to lowest
-		printf("\nEnter grade for %s\n --> ", names[i]);
-		scanf("%f", &grades[i]); 
+    for (int i = 0; i < size; ++i)
+    {
+        // ask for student name first
+        printf("\nEnter student name\n --> ");
+        scanf(" %[^\n]s", names[i]);   // space has to be in front for the scanf() to work
+        // get grades and sort from highest to lowest
+        printf("\nEnter grade for %s\n --> ", names[i]);
+        scanf("%f", &grades[i]);
 
-		if ( i >= 1) {
-			if (grades[i] < grades[i - 1]) {
-					temp = grades[i];
-					strcpy(temp_names[i], names[i]);
-					grades[i] = grades[i - 1];
-					strcpy(names[i], names[i - 1]);
-					grades[i - 1] = temp;
-					strcpy(names[i - 1], temp_names[i]);
-				}
+        if ( i >= 1) {
+            if (grades[i] < grades[i - 1]) {
+                temp = grades[i];
+                strcpy(temp_names[i], names[i]);
+                grades[i] = grades[i - 1];
+                strcpy(names[i], names[i - 1]);
+                grades[i - 1] = temp;
+                strcpy(names[i - 1], temp_names[i]);
+            }
 
-			}
+        }
 
-	}
+    }
 
-	// once all grades are entered, print formated report
-	print_report(names, grades, size);
+    // once all grades are entered, print formated report
+    print_report(names, grades, size);
 
 }
 
 /* Print a nice report and call other helper functions to get information */
 void print_report(char names[][50], float grades[], int size)
 {
-	float average, median, lowest = grades[0], highest = grades[size - 1];
-	char highest_name[50], lowest_name[50];
+    float average, median, lowest = grades[0], highest = grades[size - 1];
+    char highest_name[50], lowest_name[50];
 
-	strcpy(highest_name, names[size - 1]);
-	strcpy(lowest_name, names[0]);
+    strcpy(highest_name, names[size - 1]);
+    strcpy(lowest_name, names[0]);
 
-	printf("\n\n\n__________________________________________________\n");
-	printf("\n\tSorted Test Results");
-	printf("\n__________________________________________________\n");
+    printf("\n\n\n__________________________________________________\n");
+    printf("\n\tSorted Test Results");
+    printf("\n__________________________________________________\n");
 
-	for (int i = 0; i < size; ++i)
-	{
-		printf("\nName: %s \t\t\tGrade: %0.1f\n", names[i], grades[i]);
-	}
+    for (int i = 0; i < size; ++i)
+    {
+        printf("\nName: %s \t\t\tGrade: %0.1f\n", names[i], grades[i]);
+    }
 
-	/* call other functions to return information
-	 average, median, highest, lowest, and name of highest score / name of lowest score
-	printf("\n%s\n", );
-	printf("%s\n", );
-	printf("%s\n", );
-	printf("%s\n", );
-	*/
+    /* call other functions to return information */
+     average = average_grade(grades, size);
+     median = median_grade(grades, size);
 
-}	
+     printf("\nAverage: %.1f    Median: %.1f    Highest: %.1f    Lowest: %.1f\n", average, median, highest, lowest);
 
+}
+
+
+/* Return the average grade */
+float average_grade(float grades[], int size)
+{
+    float sum = 0;
+    float avrg;
+
+    for (int i = 0; i < size; ++i)
+    {
+        sum += grades[i];
+    }
+
+    avrg = sum / size;
+    return avrg;
+}
+
+
+/* Return median grade as a double */
+float median_grade(float grades[], int size)
+{
+    float middle_number;
+
+    // check if array is even
+    if(size % 2 == 0) {
+        middle_number = (grades[(size - 1) / 2 ] + grades[size / 2]) / 2;
+    }
+    else {
+        middle_number = grades[size / 2];
+    }
+
+    return middle_number;
+}
