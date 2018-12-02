03ee7248ebc03b9d14e8357f67ab591b123cfd36
John Glatts <33476958+jglatts@users.noreply.github.com>
2018-12-02 11:53:23 -0500

    Update assignment_five.c

--- assignment_five.c
+++ assignment_five.c
@@ -3,21 +3,23 @@
 CIS 1057 Assignment 5
 Author: John Glatts
 TA: Shi Kai Fang
-Due Date: 29 November 2018
+Due Date: 2 December 2018
 
 */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
 
 
 /* Function Prototypes */
 void enter_grades(char [][50], float[], int);
-void print_report(char [][50], char [][50], float [], float [], int);
-void temp_values(char [][50], char [][50], float [], float [], int);
+void print_report(char [][50], float [], int);
+float highest_grade(float[], int);
+float lowest_grade(float[], int);
 float average_grade(float[], int);
 float median_grade(float[], int);
+int highest_grade_name(float[], int);
+int lowest_grade_name(float[], int);
 
 
 int main(int argc, char const *argv[])
@@ -30,8 +32,7 @@ int main(int argc, char const *argv[])
     printf("\n\nHow many students are being added??\n --> ");
     scanf("%d", &student_size);
 
-    // declare string arrays for all the names
-    // have to declare these with integers, windows has a compiler error if initialize with student_size (works on linux)
+    // have to declare these with integers, windows has a compiler error if initialized with student_size
     char student_names[50][50];
     float student_grades[50];
 
@@ -45,7 +46,7 @@ int main(int argc, char const *argv[])
 /* Enter student grades and sort them as they're entered */
 void enter_grades(char names[][50], float grades[], int size)
 {
-    float temp, temp_grades[50];
+    float temp;
     char temp_names[50][50];
     char first_char_check, second_char_check;
     int grades_entered = 0;
@@ -55,14 +56,12 @@ void enter_grades(char names[][50], float grades[], int size)
         printf("\nEnter student name\n --> ");
         // space has to be in front for the scanf() to work
         scanf(" %[^\n]s", names[i]);
-        strcpy(temp_names[i], names[i]);
         // get grades and sort from highest to lowest
         printf("\nEnter grade for %s\n --> ", names[i]);
         scanf("%f", &grades[i]);
-        temp_grades[i] = grades[i];
         grades_entered++;
 
-        // sort grades as there entered, plz baby jesus
+        // sort names as there entered
         // make sure we can check a value at i-1
         if (i >= 1 ) {
             // loop through the current amount of grades entered
@@ -70,10 +69,8 @@ void enter_grades(char names[][50], float grades[], int size)
                 // loop to keep checking adjacent values
                 // change to sort by the Names, not the grades
                 for (int k = 0; k < grades_entered; ++k) {
-                    //printf("\nChecking Values %.1f and %.1f", grades[k], grades[k - 1]);
                     first_char_check = names[k][0];
                     second_char_check = names[k - 1][0];
-                    printf("\nChecking Values %c and %c", first_char_check, second_char_check);
                     if (first_char_check < second_char_check){
                         temp = grades[k];
                         strcpy(temp_names[k], names[k]);
@@ -88,14 +85,15 @@ void enter_grades(char names[][50], float grades[], int size)
 
     }
     // once all grades are entered, print formatted report
-    print_report(temp_names, names, temp_grades, grades, size);
+    print_report(names, grades, size);
 }
 
 
 /* Print a nice report and call other helper functions to get information */
-void print_report(char temp_names[][50], char names[][50],  float temp_grades[], float grades[], int size)
+void print_report(char names[][50], float grades[], int size)
 {
-    float average, median;
+    float average, median, highest, lowest;
+    int highest_name_index, lowest_name_index;
 
     printf("\n\n\n__________________________________________________\n");
     printf("\n\tSorted Test Results");
@@ -105,38 +103,19 @@ void print_report(char temp_names[][50], char names[][50],  float temp_grades[],
     {
         printf("\nName: %s \tGrade: %3.2f\n", names[i], grades[i]);
     }
-    // another sort to make it easier to find values
-    temp_values(temp_names, names, temp_grades, grades, size);
+    // call helper functions for information
+    highest_name_index = highest_grade_name(grades, size);
+    lowest_name_index = lowest_grade_name(grades, size);
+    highest = highest_grade(grades, size);
+    lowest = lowest_grade(grades, size);
+    average = average_grade(grades, size);
+    median = median_grade(grades, size);
 
-    average = average_grade(temp_grades, size);
-    median = median_grade(temp_grades, size);
-
-    // use the temp_grades array to get data
-    // fix median and average grades
-    printf("\nAverage: %3.2f    Median: %3.2f    Highest: %3.2f    Lowest: %3.2f\n", average, median, temp_grades[size - 1], temp_grades[0]);
-    printf("\nHighest Grade: %s \t\tLowest Grade: %s\n", temp_names[size-1], temp_names[0]);
+    // print grade information
+    printf("\nAverage: %3.2f    Median: %3.2f    Highest: %3.2f    Lowest: %3.2f\n", average, median, highest, lowest);
+    printf("\nHighest Grade: %s \t\tLowest Grade: %s\n", names[highest_name_index], names[lowest_name_index]);
 }
 
-
-/* Sort by grades and store in a temp. array. Easier to get data this way. */
-    void temp_values(char temp_names[][50], char names[][50], float temp_grades[], float grades[], int size){
-    float temp;
-    char secondary_temp_names[50][50];
-
-    for (int i = 0; i < size - 1; ++i) {
-        for (int j = 0; j < size - i - 1; ++j) {
-            // rework the alg. to place names in correct locations
-            if (temp_grades[j] > temp_grades[j + 1]) {
-                temp = temp_grades[j];
-                strcpy(secondary_temp_names[j], temp_names[j]);
-                temp_grades[j] = temp_grades[j + 1];
-                strcpy(temp_names[j], temp_names[j + 1]);
-                temp_grades[j + 1] = temp;
-                strcpy(temp_names[j + 1], secondary_temp_names[j]);
-            }
-        }
-    }
-}
 
 /* Return the average grade */
 float average_grade(float grades[], int size)
@@ -158,7 +137,18 @@ float average_grade(float grades[], int size)
 /* Return median grade as a float */
 float median_grade(float grades[], int size)
 {
-    float middle_number;
+    float middle_number, temp;
+
+    // sort the grades
+    for (int i = 0; i < size - 1; ++i) {
+        for (int j = 0; j < size - i - 1; ++j) {
+            if (grades[j] > grades[j + 1]) {
+                temp = grades[j];
+                grades[j] = grades[j + 1];
+                grades[j + 1] = temp;
+            }
+        }
+    }
 
     // check if array is even
     if(size % 2 == 0) {
@@ -169,4 +159,82 @@ float median_grade(float grades[], int size)
     }
 
     return middle_number;
+}
+
+
+/* Return highest grade as a float */
+float highest_grade(float grades[], int size)
+{
+    float temp;
+
+    // sort the grades
+    for (int i = 0; i < size - 1; ++i) {
+        for (int j = 0; j < size - i - 1; ++j) {
+            if (grades[j] > grades[j + 1]) {
+                temp = grades[j];
+                grades[j] = grades[j + 1];
+                grades[j + 1] = temp;
+            }
+        }
+    }
+
+    return grades[size - 1];
+}
+
+
+/* Return lowest grade as a float */
+float lowest_grade(float grades[], int size)
+{
+    float temp;
+
+    // sort the grades
+    for (int i = 0; i < size - 1; ++i) {
+        for (int j = 0; j < size - i - 1; ++j) {
+            if (grades[j] > grades[j + 1]) {
+                temp = grades[j];
+                grades[j] = grades[j + 1];
+                grades[j + 1] = temp;
+            }
+        }
+    }
+
+    return grades[0];
+}
+
+
+/* Return the index of the highest grade
+ * print_report() will use the index to display the correct name
+ * */
+int highest_grade_name(float grades[], int size) {
+    int high_idx = 0;
+
+    // find the index of the highest grades
+    for (int i = 0; i < size - 1; ++i) {
+        for (int j = 0; j < size - 1; ++j) {
+            if (grades[j] > grades[high_idx]) {
+                high_idx = j;
+            }
+        }
+    }
+
+    return high_idx;
+}
+
+
+/* Return the index of the lowest grade
+ * print_report() will use the index to display the correct name
+ * */
+int lowest_grade_name(float grades[], int size) {
+    int low_idx = 0;
+
+    // find the index of the highest grades
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            if (grades[j] < grades[low_idx]) {
+                low_idx = j;
+            }
+        }
+    }
+
+    return low_idx;
 }
